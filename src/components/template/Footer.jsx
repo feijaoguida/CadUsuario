e2a54b650c8a167734eb843e2f998c4833659a86
feijaoguida <feijaoguida@hotmail.com>
2020-05-04 21:41:35 -0300

    Stilizando footer

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -2,4 +2,12 @@ import React from "react";
 
 import "./Footer.css";
 
-export default (props) => <footer className="footer">Rodapé</footer>;
+export default (props) => (
+  <footer className="footer">
+    Desenvolvidor com <i className="fa fa-heart text-danger"></i> por
+    <strong>
+      {" "}
+      <span className="text-danger">RBG</span>Soluções{" "}
+    </strong>
+  </footer>
+);
